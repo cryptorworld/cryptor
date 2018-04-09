914e5252dca61847c4f8489ad74c32fc0a4a810a
insideoutzombie <david@web3devs.com>
2018-04-09 14:30:07 -0500

    updated ip address Line 110

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,7 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "45.63.6.19:17236",
-  "144.202.95.240:18236"
+  "144.202.95.240:17236"
 };
 
 struct CheckpointData {
